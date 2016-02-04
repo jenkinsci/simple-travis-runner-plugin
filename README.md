66ba5dbdcc18b5be4283665aee496a49198625d0
Andrew Bayer <andrew.bayer@gmail.com>
2016-02-04 14:39:39 -0800

    Added help.jelly, fail out run(...) if called outside a node block.

--- README.md
+++ README.md
@@ -17,10 +17,10 @@ Actual guts are all in [TravisPipelineConverter.groovy](https://github.com/abaye
 An example of using a Jenkinsfile to call a .travis.yml can be found at [this repo](https://github.com/abayer/dummy-travis-test).
 
 # TODO
-- [ ] config.jelly and help HTML for Snippet Generator and reference docs.
+- [X] ~~config.jelly and~~ \(config.jelly not needed for global variables\) help HTML for Snippet Generator and reference docs.
 - [ ] Tests, tests, tests!
 - [ ] Ideally, find a way to optionally output as Pipeline code rather than execute, for migration purposes.
-- [ ] Make sure this will fail if run outside of a `node` block.
+- [X] Make sure this will fail if run outside of a `node` block.
 - [ ] Allow execution of all `script` entries even if one fails.
 - [ ] Time out individual `script` entries.
 - [ ] Determine what language-specific environment axes will be supported natively (i.e., `rvm`, `php`, etc)
--- src/main/resources/org/jenkinsci/plugins/travispipelineconverter/TravisPipelineConverter.groovy
+++ src/main/resources/org/jenkinsci/plugins/travispipelineconverter/TravisPipelineConverter.groovy
@@ -13,83 +13,87 @@ class TravisPipelineConverter implements Serializable {
 
     /**
      * Load a ".travis.yml" file from the given path in the current workspace and execute it as best as we can.
-     * TODO: make sure this can only run within a node { }. Not sure how to do that here.
+     * Errors out if run outside a node { } block.
      *
      * @param path The path to the file in question.
      */
     public void run(String path) {
-        String travisFile = script.readFile(path)
+        if (script.env.HOME == null) {
+            script.error("travisPipelineConverter.run(...) can only be run within a 'node { ... }' block.")
+        } else {
+            String travisFile = script.readFile(path)
 
-        def travisSteps = readAndConvertTravis(travisFile)
+            def travisSteps = readAndConvertTravis(travisFile)
 
-        // Fail fast on any errors in before_install, install or before_script
-        if (travisSteps.containsKey("before_install")) {
-            script.stage "Travis Before Install"
-            getSteps(travisSteps.get("before_install")).call()
+            // Fail fast on any errors in before_install, install or before_script
+            if (travisSteps.containsKey("before_install")) {
+                script.stage "Travis Before Install"
+                getSteps(travisSteps.get("before_install")).call()
 
-        }
-        if (travisSteps.containsKey("install")) {
-            script.stage "Travis Install"
-            getSteps(travisSteps.get("install")).call()
-
-        }
-        if (travisSteps.containsKey("before_script")) {
-            script.stage "Travis Before Script"
-            getSteps(travisSteps.get("before_script")).call()
-        }
-
-        // Note any failure in the script section but don't fail the build yet.
-        def failedScript = false
-        try {
-            // TODO: Ideally we change this to note failures in script steps but continue through all of them
-            // to completion anyway, as described in https://docs.travis-ci.com/user/customizing-the-build/#Customizing-the-Build-Step,
-            // but I want to think about the implementation more.
-            // TODO: Add timeout support (https://docs.travis-ci.com/user/customizing-the-build/#Build-Timeouts) for individual
-            // script/test suite steps.
-            if (travisSteps.containsKey("script")) {
-                script.stage "Travis Script"
-                getSteps(travisSteps.get("script")).call()
             }
-        } catch (Exception e) {
-            script.echo("Error on script step: ${e}")
-            failedScript = true
-        }
+            if (travisSteps.containsKey("install")) {
+                script.stage "Travis Install"
+                getSteps(travisSteps.get("install")).call()
 
-        if (!failedScript) {
-            // Skip the deploy-related steps since those rely on Travis internals.
-            for (String deployStep in ["before_deploy", "deploy", "after_deploy"].toSet()) {
-                if (travisSteps.containsKey(deployStep)) {
-                    script.echo("Not executing '${deployStep}' - Travis-specific")
+            }
+            if (travisSteps.containsKey("before_script")) {
+                script.stage "Travis Before Script"
+                getSteps(travisSteps.get("before_script")).call()
+            }
+
+            // Note any failure in the script section but don't fail the build yet.
+            def failedScript = false
+            try {
+                // TODO: Ideally we change this to note failures in script steps but continue through all of them
+                // to completion anyway, as described in https://docs.travis-ci.com/user/customizing-the-build/#Customizing-the-Build-Step,
+                // but I want to think about the implementation more.
+                // TODO: Add timeout support (https://docs.travis-ci.com/user/customizing-the-build/#Build-Timeouts) for individual
+                // script/test suite steps.
+                if (travisSteps.containsKey("script")) {
+                    script.stage "Travis Script"
+                    getSteps(travisSteps.get("script")).call()
+                }
+            } catch (Exception e) {
+                script.echo("Error on script step: ${e}")
+                failedScript = true
+            }
+
+            if (!failedScript) {
+                // Skip the deploy-related steps since those rely on Travis internals.
+                for (String deployStep in ["before_deploy", "deploy", "after_deploy"].toSet()) {
+                    if (travisSteps.containsKey(deployStep)) {
+                        script.echo("Not executing '${deployStep}' - Travis-specific")
+                    }
                 }
             }
-        }
 
-        // Swallow any errors in after_*.
-        try {
-            // If the script failed, proceed to after_failure.
+            // Swallow any errors in after_*.
+            try {
+                // If the script failed, proceed to after_failure.
+                if (failedScript) {
+                    if (travisSteps.containsKey("after_failure")) {
+                        script.stage "Travis After Failure"
+                        getSteps(travisSteps.get("after_failure")).call()
+                    }
+                } else {
+                    // Otherwise, check after_success.
+                    if (travisSteps.containsKey("after_success")) {
+                        script.stage "Travis After Success"
+                        getSteps(travisSteps.get("after_success")).call()
+                    }
+                }
+                if (travisSteps.containsKey("after_script")) {
+                    script.stage "Travis After Script"
+                    getSteps(travisSteps.get("after_script")).call()
+                }
+            } catch (Exception e) {
+                script.echo("Error on after step(s), ignoring: ${e}")
+            }
+
+            // If we saw a failure in the script step earlier, error out now.
             if (failedScript) {
-                if (travisSteps.containsKey("after_failure")) {
-                    script.stage "Travis After Failure"
-                    getSteps(travisSteps.get("after_failure")).call()
-                }
-            } else {
-                // Otherwise, check after_success.
-                if (travisSteps.containsKey("after_success")) {
-                    script.stage "Travis After Success"
-                    getSteps(travisSteps.get("after_success")).call()
-                }
+                script.error("Failing build due to failure of script step.")
             }
-            if (travisSteps.containsKey("after_script")) {
-                script.stage "Travis After Script"
-                getSteps(travisSteps.get("after_script")).call()
-            }
-        } catch (Exception e) {
-            script.echo("Error on after step(s), ignoring: ${e}")
-        }
-
-        // If we saw a failure in the script step earlier, error out now.
-        if (failedScript) {
-            script.error("Failing build due to failure of script step.")
         }
     }
 
